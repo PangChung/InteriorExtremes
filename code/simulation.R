d82a2079d2d753c56187228df51c41c71d929885
PangChung <zhongprw@gmail.com>
2023-09-18 14:43:36 +1000

    add simulation code

--- code/simulation.R
+++ code/simulation.R
@@ -5,6 +5,8 @@
 # Simulate a truncated extremal-t max-stable process
 # @n : number of observations
 # @par : parameters of the model
+    # @par[[1]] : nu: degrees of freedom
+    # @par[[2]] : sigma: correlation matrix
 simu_truncT <- function(m,par,parallel=TRUE,ncores){    
     nu = par[[1]]
     n = nrow(sigma)
@@ -20,8 +22,25 @@ simu_truncT <- function(m,par,parallel=TRUE,ncores){
     sigma.list = lapply(T_j,function(x) x[[2]])
     a_j = T_j_val-log(phi)+log(2)*((nu-2)/2)+gamma_1-1/2*log(pi)
     
-    Z = matrix(0,nrow=n,ncol=m)
-    r = rexp(m)
+    # Simulate the max-stable process
+    func <- function(idx){
+        r = rexp(1)
+        z = rep(0,n)
+        for(j in 1:n){
+            while(1/r > z[j]){
+                z_temp = rep(1,n)
+                z_temp[-j] = tmvtsim(n=1,k=d-1, means=sigma[-j,j], sigma=sigma.list[[j]], lower=rep(0,d),upper=rep(Inf,d),df=nu+1)[[1]]
+                z_temp[-j] = z_temp[-j]^nu * a[j] / a[-j]
+                z_temp = z_temp / r
+                if(any(! z_temp[0:j] < z[0:j])){
+                    z = pmax(z,z_temp)
+                }else{
+                    r = r + rexp(1)
+                }
+                
+            }
+        }
+    }
     
     
     
@@ -35,17 +54,16 @@ Sigma <- matrix(0, nrow=d, ncol=d)
 Sigma <- rho^abs(row(Sigma) - col(Sigma))
 D1 <- diag(1,d) # Full rank
 set.seed(1203)
-t0 <- system.time(ans.1 <- tmvmixnorm::rtmvn(n=1, Mean=rep(0,d), Sigma, lower=rep(0,d),int=rep(0.1,d),upper=rep(Inf,d), burn=50))[3]
+t0 <- system.time(ans.1 <- tmvmixnorm::rtmvn(n=1, Mean=rep(0,d), Sigma, lower=rep(0,d),int=rep(0.1,d),upper=rep(Inf,d), burn=0))[3]
 
 t1 <- proc.time()
 ans.2 <- mvtnorm::rmvnorm(n=1, mean=rep(0,d), Sigma, method="chol")
 t2 <- (proc.time() - t1)[3]
 
+t3 <- system.time(ans.4 <- tmvmixnorm::rtmvt(n=1, nu=10, Mean=rep(0,d), Sigma, lower=rep(0,d),D=diag(1,d),int=rep(0.1,d),upper=rep(Inf,d), burn=0))[3] ## worst performance
 
-system.time(ans.3 <- mvnfast::rmvt(n=10000, mu=rep(0,d),sigma=Sigma, df=10, ncores=1))
-any(!apply(ans.3,1,function(x){any(x<0)}))
-
-t0 <- system.time(ans.1 <- tmvmixnorm::rtmvt(n=1, nu=10, Mean=rep(0,d), Sigma, lower=rep(0,d),D=diag(1,d),int=rep(0.1,d),upper=rep(Inf,d), burn=0))[3]
+t4 <- system.time(ans.5 <- tmvnsim(n=1,k=d, means=rep(0,d), sigma=Sigma, lower=rep(0,d),upper=rep(Inf,d))[[1]])[3] ## certainly this is the best
+t4 <- system.time(ans.5 <- tmvtsim(n=1,k=d,df=10, means=rep(0,d), sigma=Sigma, lower=rep(0,d),upper=rep(Inf,d))[[1]])[3] ## certainly this is the best
 
 # Simulate a log-skew normal based max-stable process
 # @n : number of observations
@@ -53,5 +71,5 @@ t0 <- system.time(ans.1 <- tmvmixnorm::rtmvt(n=1, nu=10, Mean=rep(0,d), Sigma, l
 simu_logskew <- function(n,par,parallel=TRUE,ncores){  
     alpha = par[[1]]; sigma = par[[2]]
     n = nrow(sigma)
-    
+
 }
