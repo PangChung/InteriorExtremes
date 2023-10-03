2b5859c819fb41e8dbe5b2d5bbab2ceb0af08703
PangChung <zhongprw@gmail.com>
2023-10-03 15:43:05 +1100

    add extremal coef plot

--- code/simulation_test.R
+++ code/simulation_test.R
@@ -35,19 +35,31 @@ ec.trunc <- apply(all.pairs,2,empirical_extcoef,data=Z.trunc)
 tc.truncT1 <- true_extcoef(all.pairs,par=par1,model="truncT1")
 tc.truncT2 <- mcmapply(true_extcoef,all.pairs.list,MoreArgs=list(par=par1,model="truncT2"),mc.cores=10)
 
-plot(x=diff.mat[t(all.pairs)],y=ec.trunc,type="p",cex=0.5,ylim=c(1,2),xlab="Distance",ylab="Extremal Coefficient",pch=20)
+pdf("figures/extcoef_truncT.pdf",width=6,height=4)
+par(mfrow=c(1,1),mar=c(4,4,2,1),cex.main=1,cex.lab=1,mgp=c(2,1,0))
+plot(x=diff.mat[t(all.pairs)],y=ec.trunc,type="p",cex=0.5,ylim=c(1,2),xlab="Distance",ylab="Extremal Coefficient",
+    main="Truncated extremal t processes",pch=20)
 points(x=diff.mat[t(all.pairs)],y=tc.truncT1,type="p",cex=0.5,col="red",pch=20)
-points(x=diff.mat[t(all.pairs)],y=tc.truncT2,type="p",cex=0.5,col="blue",pch=20)
-abline(h=c(1,2),col="grey",lty=2)
+points(x=diff.mat[t(all.pairs)],y=tc.truncT2,type="p",cex=0.5,col="#0000ff72",pch=20)
+abline(h=c(1,2),col="grey",lty=2,cex=2)
+legend("topleft",legend=c("Emperical","Method 1","Method 2"),col=c("black","red","#0000ff72"),
+    bty="n",pch=20,cex=1)
+dev.off()
 
 ec.logskew <- apply(all.pairs,2,empirical_extcoef,data=Z.logskew)
 tc.logskew1 <- mcmapply(true_extcoef,all.pairs.list,MoreArgs=list(par=par2,model="logskew1"),mc.cores=10)
 tc.logskew2 <- mcmapply(true_extcoef,all.pairs.list,MoreArgs=list(par=par2,model="logskew2"),mc.cores=10)
-plot(x=diff.mat[t(all.pairs)],y=ec.logskew,type="p",cex=0.5,ylim=c(1,2),xlab="Distance",ylab="Extremal Coefficient",pch=20)
-points(x=diff.mat[t(all.pairs)],y=tc.logskew1,type="p",cex=0.5,col="red",pch=20)
-points(x=diff.mat[t(all.pairs)],y=tc.logskew2,type="p",cex=0.5,col="#7eb3d8f2",pch=20)
-abline(h=c(1,2),col="grey",lty=2)
 
+pdf("figures/extcoef_logskew.pdf",width=6,height=4)
+par(mfrow=c(1,1),mar=c(4,4,2,1),cex.main=1,cex.lab=1,mgp=c(2,1,0))
+plot(x=diff.mat[t(all.pairs)],y=ec.logskew,type="p",cex=0.5,ylim=c(1,2),xlab="Distance",ylab="Extremal Coefficient",
+    main = "Log-skew normal based max-stable processes",pch=20)
+points(x=diff.mat[t(all.pairs)],y=tc.logskew1,type="p",cex=0.5,col="#ff0000a8",pch=20)
+points(x=diff.mat[t(all.pairs)],y=tc.logskew2,type="p",cex=0.5,col="#7eb3d8f2",pch=20)
+abline(h=c(1,2),col="grey",lty=2,cex=2)
+legend("topleft",legend=c("Emperical","Method 1","Method 2"),col=c("black","#ff0000a8","#7eb3d8f2"),
+    bty="n", pch=20,cex=1)
+dev.off()
 
 
 
