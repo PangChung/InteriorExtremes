2108d6384d956a0e95e1a1be5ca05928d49e06d6
PangChung <zhongprw@gmail.com>
2024-02-07 11:53:57 +1100

    bug fix

---
+++ code/katana2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -l ncpus=16
+#PBS -l mem=124gb
+#PBS -l walltime=12:00:00
+#PBS -j oe 
+#PBS -J 1-300
+##PBS -m ae 
+##PBS -M peng.zhong@unsw.edu.au 
+
+cd ${PBS_O_WORKDIR}
+
+module load gsl/2.7.1 
+module load gmp/6.2.1 
+module load r/4.3.1
+
+Rscript $script "${inputs};id=${PBS_ARRAY_INDEX}"
+
---
+++ code/simulation_studies_vecchia.R
@@ -0,0 +1,57 @@
+args <- commandArgs(TRUE)
+.libPaths("../src")
+library(parallel)
+library(mvtnorm)
+library(TruncatedNormal)
+library(evd)
+library(partitions)
+library(Rfast)
+library(matrixStats)
+library(splines)
+source("code/simulation.R")
+source("code/exponent_functions.R")
+source("code/likelihood_inference.R")
+
+id = 1
+ncores=detectCores()
+d <- 10 ## 10 * 10 grid on [0,1]^2
+m <- 1000 ## number of samples
+set.seed(1342342)
+coord = as.matrix(expand.grid(0:(d-1),0:(d-1))/d)
+diff.vector <- cbind(as.vector(outer(coord[,1],coord[,1],'-')),as.vector(outer(coord[,2],coord[,2],'-'))) 
+diff.mat <- matrix(apply(diff.vector, 1, function(x) sqrt(sum(x^2))), ncol=nrow(coord))
+para.range = c(0.5,1,2) ## range for the correlation function ##
+para.nu = c(0.5,1,1.5) ## smoothness parameter for the correlation function ##
+para.alpha = rbind(c(0,0,0),c(-1,2,3),c(-2,-1,4)) ## slant parameter for skewed norm model ##
+para.deg = 2 ## degree of the freedom for the truncated t model ##
+all.pairs = combn(1:nrow(coord),2)
+all.pairs.list = split(all.pairs,col(all.pairs))
+for (arg in args) eval(parse(text = arg))
+file2save = paste0("/srv/scratch/z3536974/data/simulation_study_vecchia_",id,"_",thres*100,".RData")
+init.seed = as.integer((as.integer(Sys.time())/id + sample.int(10^5,1))%%10^5)
+set.seed(init.seed)
+vecchia.seq <- 1:nrow(coord)#sample(1:nrow(coord),size=nrow(coord),replace=FALSE)
+neighbours.mat <- sapply(1:nrow(coord),FUN=neighbours,vecchia.seq=vecchia.seq,
+					q=3,loc=diff.mat)
+lb=c(0.01,0.01,-Inf,-Inf,-Inf)
+ub=c(10,2.0,Inf,Inf,Inf)
+init = c(1,0.5,-0.1,0.1,0.1)
+
+########################################################################
+### simulation study for the log-skew normal based max-stable process ##
+########################################################################
+par.skew.normal <- as.matrix(expand.grid(para.range,para.nu,1:3))
+par.skew.normal <- cbind(par.skew.normal[,-3],para.alpha[par.skew.normal[,3],]);colnames(par.skew.normal) <- NULL
+samples.skew.normal <- list()
+par.skew.list <- list()
+ec.logskew <- list()
+tc.logskew <- list()
+fit.logskew.vecchia <- list()
+for(i in 1:nrow(par.skew.normal)){
+    par.skew.list[[i]] <- list(sigma=cov.func(coord,par.skew.normal[i,1:2]),alpha=alpha.func(coord,par.skew.normal[i,-c(1:2)]))
+    samples.skew.normal[[i]] <- simu_logskew(m=m,par=alpha2delta(par.skew.list[[i]]),ncores=ncores)
+    fit.logskew.vecchia[[i]] <- MVLE(data=samples.skew.normal[[i]],init=par.skew.normal[i,],fixed=c(F,F,F,F,F),loc=coord,FUN=cov.func,vecchia.seq=vecchia.seq,neighbours = neighbours.mat,alpha.func=alpha.func,maxit=200,model="logskew",lb=lb,ub=ub,ncores=ncores)
+}
+
+save(fit.logskew.vecchia,file=file2save)
+
