632abc913fd636489cd50f91d79d9dadfadbbac7
PangChung <zhongprw@gmail.com>
2024-02-14 15:36:49 +1100

    bug fix

--- code/simulation_studies.R
+++ code/simulation_studies.R
@@ -15,8 +15,8 @@ switch(computer,
 coord = as.matrix(expand.grid(0:(d-1),0:(d-1))/d)
 diff.vector <- cbind(as.vector(outer(coord[,1],coord[,1],'-')),as.vector(outer(coord[,2],coord[,2],'-'))) 
 diff.mat <- matrix(apply(diff.vector, 1, function(x) sqrt(sum(x^2))), ncol=nrow(coord))
-para.range = c(1,2)#c(0.5,1,2) ## range for the correlation function ##
-para.nu = c(0.5,1)#c(0.5,1,1.5) ## smoothness parameter for the correlation function ##
+para.range = c(0.5,1,2) ## range for the correlation function ##      
+para.nu = c(0.5,1,1.5) ## smoothness parameter for the correlation function ##
 para.alpha = rbind(c(0,0,0),c(-1,2,3),c(-2,-1,4)) ## slant parameter for skewed norm model ##
 para.deg = c(2,3) ## degree of the freedom for the truncated t model ##
 all.pairs = combn(1:nrow(coord),2)
