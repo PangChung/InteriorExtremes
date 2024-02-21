94ab5f5f8b8da87eecfa8c5bf336f034d3ee114d
PangChung <zhongprw@gmail.com>
2024-02-21 13:43:16 +1100

    update

--- code/simulation_studies.R
+++ code/simulation_studies.R
@@ -41,6 +41,7 @@ ncores=detectCores()
 file2save = paste0(DataPath,"data/simulation_study_",model,"_",id,"_",m,".RData")
 init.seed = as.integer((as.integer(Sys.time())/id + sample.int(10^5,1))%%10^5)
 set.seed(init.seed)
+
 ##compute the basis ###
 # centers <- rbind(c(0.5,0.5),c(0.25,0.25),c(0.75,0.75))
 # idx.centers <- apply(centers,1,function(x){which.min(apply(coord,1,function(y){sum((x-y)^2)}))})
