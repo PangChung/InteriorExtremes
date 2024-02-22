7749ae0c5c3b033ca08869a0e6abe308187312b6
PangChung <zhongprw@gmail.com>
2024-02-22 13:44:12 +1100

    bug fix

--- code/simulation_studies_comp.R
+++ code/simulation_studies_comp.R
@@ -1,6 +1,6 @@
 args <- commandArgs(TRUE)
 id = 1
-computer = "ws"
+computer = "hpc"
 d <- 15 ## 10 * 10 grid on [0,1]^2
 m <- 100 ## number of samples
 # loading library and setting path
@@ -26,28 +26,28 @@ ncores=detectCores()
 coord = as.matrix(expand.grid(0:(d-1),0:(d-1))/d)
 diff.vector <- cbind(as.vector(outer(coord[,1],coord[,1],'-')),as.vector(outer(coord[,2],coord[,2],'-'))) 
 diff.mat <- matrix(apply(diff.vector, 1, function(x) sqrt(sum(x^2))), ncol=nrow(coord))
-para.range = c(0.5,1,2) ## range for the correlation function ##
+para.range = c(0.5,1) ## range for the correlation function ##
 para.nu = c(0.5,1) ## smoothness parameter for the correlation function ##
-para.alpha = rbind(c(0,0,0),c(1,1,1),c(-1,-1,-1),c(1,2,-3)) ## slant parameter for skewed norm model ##
+para.alpha = rbind(c(0,0),c(-1,-2),c(-2,-1),c(2,1)) 
 para.deg = c(2,3) ## degree of the freedom for the truncated t model ##
 all.pairs = combn(1:nrow(coord),2)
 all.pairs.list = split(all.pairs,col(all.pairs))
-file2save = paste0(DataPath,"data/simulation_study_vecchia_",id,".RData")
+file2save = paste0(DataPath,"data/simulation_study_comp_",id,".RData")
 init.seed = as.integer((as.integer(Sys.time())/id + sample.int(10^5,1))%%10^5)
 set.seed(init.seed)
 vecchia.seq <- 1:nrow(coord)#sample(1:nrow(coord),size=nrow(coord),replace=FALSE)
 neighbours.mat <- sapply(1:nrow(coord),FUN=neighbours,vecchia.seq=vecchia.seq,
 					q=2,loc=diff.mat)
-lb=c(0.01,0.01,-Inf,-Inf,-Inf)
-ub=c(10,2.0,Inf,Inf,Inf)
-init = c(1,0.5,0,0,0)
+lb=c(0.01,0.01,rep(-Inf,ncol(para.alpha)))
+ub=c(10,2.0,rep(Inf,ncol(para.alpha)))
+init = c(1,1,0,0)
 
 ##compute the basis ###
 centers <- rbind(c(0.5,0.5),c(0.25,0.25),c(0.75,0.75))
 idx.centers <- apply(centers,1,function(x){which.min(apply(coord,1,function(y){sum((x-y)^2)}))})
 basis <- sapply(idx.centers,function(x){ y=dnorm(diff.mat[x,],mean=0,sd=0.125);y=y-mean(y) })
 
-pairs.idx = rank(diff.mat[t(all.pairs)]) < 2000
+pairs.idx = rank(diff.mat[t(all.pairs)]) < 3000
 ########################################################################
 ### simulation study for the log-skew normal based max-stable process ##
 ########################################################################
@@ -64,8 +64,9 @@ for(i in 1:nrow(par.skew.normal)){
     samples.skew.normal[[i]] <- simu_logskew(m=m,par=alpha2delta(par.skew.list[[i]]),ncores=ncores)
     #fit.logskew.vecchia[[i]] <- MVLE(data=samples.skew.normal[[i]],init=par.skew.normal[i,],fixed=c(F,F,F,F,F),loc=coord,FUN=cov.func,vecchia.seq=vecchia.seq,neighbours = neighbours.mat,alpha.func=alpha.func,maxit=500,model="logskew",lb=lb,ub=ub,ncores=ncores)
     print(par.skew.normal[i,])
-    fit.logskew.comp[[i]] <- MCLE(data=samples.skew.normal[[i]],init=init,fixed=c(F,F,F,F,F),loc=coord,FUN=cov.func,index=all.pairs[,pairs.idx],alpha.func=alpha.func,model="logskew",lb=lb,ub=ub,ncores=ncores,maxit=500)
+    init = par.skew.normal[i,]
+    fit.logskew.comp[[i]] <- MCLE(data=samples.skew.normal[[i]],init=init,fixed=rep(F,length(init)),loc=coord,FUN=cov.func,index=all.pairs[,pairs.idx],alpha.func=alpha.func,model="logskew",lb=lb,ub=ub,ncores=ncores,maxit=500)
 }
 
-save(fit.logskew.comp,par.skew.normal,file=file2save)
+save(fit.logskew.comp,par.skew.normal,init.seed,file=file2save)
 
