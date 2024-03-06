8890bb6aa4a46d7205f246fa70f595c21b0959aa
PangChung <zhongprw@gmail.com>
2024-03-06 20:23:33 +1100

    update

--- code/simulation_studies.R
+++ code/simulation_studies.R
@@ -79,11 +79,12 @@ if(model == "logskew"){
             init.mat = cbind(fit.result1$par[1],fit.result1$par[2],a)
             init.list = split(init.mat,row(init.mat))
 
-            fit.result = mcmapply(FUN=fit.model,init=init.list,MoreArgs=list(data=samples.skew.normal[[i]],loc=coord,fixed=c(F,F,F,F),thres=thres[j],model="logskew",FUN=cov.func,alpha.func=alpha.func,ncores=NULL,maxit=1000,method="Nelder-Mead",lb=lb,ub=ub,bootstrap=FALSE,hessian=FALSE,opt=TRUE,trace=FALSE),mc.set.seed = FALSE,mc.cores=ncores,SIMPLIFY = FALSE)
+            # fit.result = mcmapply(FUN=fit.model,init=init.list,MoreArgs=list(data=samples.skew.normal[[i]],loc=coord,fixed=c(T,T,F,F),thres=thres[j],model="logskew",FUN=cov.func,alpha.func=alpha.func,ncores=NULL,maxit=1000,method="Nelder-Mead",lb=lb,ub=ub,bootstrap=FALSE,hessian=FALSE,opt=TRUE,trace=FALSE),mc.set.seed = FALSE,mc.cores=ncores,SIMPLIFY = FALSE)
 
-            results.mat <- unlist(lapply(fit.result,function(x){c(x$value)}))
-            fit.logskew[[j]] = fit.result[[which.min(results.mat)]]
-            fit.logskew2[[j]] = fit.result 
+            # results.mat <- unlist(lapply(fit.result,function(x){c(x$value)}))
+            # fit.logskew[[j]] = fit.result[[which.min(results.mat)]]
+            # fit.logskew2[[j]] = fit.result 
+            fit.result[[j]] <- fit.result1
             print(c(i,j))
         }
         fit.logskew.angular[[i]] <- fit.logskew
