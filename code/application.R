82a76729fed4d6f082a961a64ba1cbb0f07b6368
PangChung <zhongprw@gmail.com>
2024-03-28 00:51:48 +1100

    update

--- code/application.R
+++ code/application.R
@@ -22,7 +22,7 @@ set.seed(12342)
 load("data/data_application.RData")
 D = ncol(maxima.frechet)
 ncores = floor(detectCores()/2)
-idx.centers = unlist(lapply(quantile(loc.sub.trans[,1],c(0.2,0.5,0.8)),function(x){ idx = abs(loc.sub.trans[,1] - x) < 5; which(idx)[which.min(abs(loc.sub.trans[idx,2] - median(loc.sub.trans[idx,2])))]}))
+idx.centers = unlist(lapply(quantile(loc.sub.trans[,1],c(0.15,0.5,0.85)),function(x){ idx = abs(loc.sub.trans[,1] - x) < 5; which(idx)[which.min(abs(loc.sub.trans[idx,2] - median(loc.sub.trans[idx,2])))]}))
 
 #plot(x=loc.sub.trans[,1],y=loc.sub.trans[,2],xlab="x",ylab="y",main="Data",pch=20)
 #points(x=loc.sub.trans[idx.centers,1],y=loc.sub.trans[idx.centers,2],col="red",pch=20)
@@ -30,7 +30,7 @@ idx.centers = unlist(lapply(quantile(loc.sub.trans[,1],c(0.2,0.5,0.8)),function(
 loc.sub.trans = apply(loc.sub.trans,2,function(x) x-mean(x))
 basis <- matrix(0,nrow=D,ncol=4)
 basis[1:floor(D/2),1] = 0.1;basis[(D-floor(D/2)+1):D,1] = -0.1
-basis[,-1] <- sapply(idx.centers,function(x){y=dnorm(distmat[,x],mean=0,sd=500);y=y-mean(y);y/sqrt(sum(y^2))})
+basis[,-1] <- sapply(idx.centers,function(x){y=-distmat[,x]/max(distmat[,x]);y=y-mean(y)})
 
 switch(id,
     results1 <- fit.model(data=maxima.frechet,loc=distmat,init=c(60,1.2,0,0,0),fixed=c(F,F,F,F,F),basis=basis,thres=0.9,model="logskew",maxit=1000,FUN=cov.func,alpha.func=alpha.func,ncores=ncores,method="L-BFGS-B",lb=c(0.01,0.0,rep(-Inf,3)),ub=c(Inf,1.99,rep(Inf,3)),hessian=FALSE,opt=TRUE,trace=TRUE), 
