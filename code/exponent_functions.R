2b5c0b661e95aa52ec1df7868d59aa94b0ed3af3
PangChung <zhongprw@gmail.com>
2024-04-10 22:16:19 +1000

    update

--- code/exponent_functions.R
+++ code/exponent_functions.R
@@ -703,7 +703,7 @@ fit.model <- function(data,loc,init,fixed=NULL,thres = 50,model="truncT",maxit=1
                 a = seq(0,2*pi,length.out=ncores)
                 a = cbind(cos(a),sin(a))
             } else {
-                a = matrix(rnorm(ncores*5*n.alpha),ncol=n.alpha)
+                a = matrix(rnorm(ncores*n.alpha),ncol=n.alpha)
                 a = sweep(a,1,sqrt(rowSums(a^2)),FUN="/")
             }
             init[!fixed] = opt.result$par
