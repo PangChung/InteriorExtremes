f88d44fb9eb49a69e929dd9ccd4273e617295dd6
PangChung <zhongprw@gmail.com>
2024-05-02 19:31:58 +1000

    update

--- code/explore.R
+++ code/explore.R
@@ -264,7 +264,7 @@ sum(idx)
 
 source("code/exponent_functions.R")
 set.seed(1)
-x=c(1,2)
+x=c(20,1)
 coord = cbind(1,c(1,5))
 par.logskew = alpha2delta(list(vario.func(coord,c(2,1)),c(-2,2)))
 par.logskew = alpha2delta(list(matrix(c(1,0.5,0.5,1),2,2),c(-2,2)))
