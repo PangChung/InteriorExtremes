e5f02445fb962d59573d40e8f32edac5420a85b5
PangChung <zhongprw@gmail.com>
2024-05-24 01:03:13 +1000

    update

--- code/exponent_functions.R
+++ code/exponent_functions.R
@@ -698,10 +698,9 @@ fit.model.pareto <- function(data,loc,init,fixed=NULL,thres = 50,model="truncT",
     t0 <- proc.time()
     n = ncol(data)
     data.sum = apply(data,1,sum)
-    idx.thres = data.sum > thres*n #& data.sum < 1000*n 
+    idx.thres = data.sum > thres 
     print(paste("#sampels: ",sum(idx.thres)))
     if(sum(idx.thres)<3){idx.thres = which(order(data.sum,decreasing=TRUE)<=3)}
-    # data = sweep(data[idx.thres,],1,data.sum[idx.thres],FUN="/")
     data = data[idx.thres,]
     if(is.null(fixed)){fixed = rep(FALSE,length(init))}
     if(is.null(lb)){lb=rep(-Inf,length(init))}
