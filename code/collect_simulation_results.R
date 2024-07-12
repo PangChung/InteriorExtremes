d9150745ec3e290bc65cd1681601e836418bb3bc
PangChung <zhongprw@gmail.com>
2024-07-12 12:25:27 +1000

    update

--- code/collect_simulation_results.R
+++ code/collect_simulation_results.R
@@ -8,6 +8,7 @@ library(tidyr)
 
 files.list1 <- list.files(path="data/simulation_comp2/",pattern=paste0("simulation_comp2_logskew_\\d+_2000_1.RData"),full.names=TRUE,recursive=FALSE)
 files.list2 <- list.files(path="data/simulation_comp2/",pattern=paste0("simulation_comp2_2_logskew_\\d+_2000_1.RData"),full.names=TRUE,recursive=FALSE)
+files.list2 <- list.files(path="data/simulation_comp3/",pattern=paste0("simulation_comp2_2_logskew_\\d+_500_1.RData"),full.names=TRUE,recursive=FALSE)
 load(files.list2[[1]],e<-new.env())
 par.skew.normal = e$par.skew.normal[,-3]
 n1 = nrow(par.skew.normal);n2 = 1 #length(e$fit.logskew.angular[[1]])
