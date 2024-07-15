d8fdc8e454100276bfbfafa33e41138ecb5ffb93
PangChung <zhongprw@gmail.com>
2024-07-15 21:54:45 +1000

    update

--- code/application.R
+++ code/application.R
@@ -45,7 +45,9 @@ idx.para = 1:2
 thres = 10
 
 results2 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=init,fixed=c(F,F,rep(F,n.alpha)),basis=basis,thres=thres,model="logskew",maxit=1000,FUN=vario.func,alpha.func=alpha.func,ncores=ncores,method="Nelder-Mead",lb=c(0.01,0.0,rep(-Inf,n.alpha)),ub=c(Inf,1.99,rep(Inf,n.alpha)),hessian=FALSE,opt=TRUE,trace=TRUE,step2=TRUE,idx.para=1:2)
+results2 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=init,fixed=c(F,F,rep(F,n.alpha)),basis=basis,thres=thres,model="logskew",maxit=1000,FUN=vario.func,alpha.func=alpha.func,ncores=ncores,method="Nelder-Mead",lb=c(0.01,0.0,rep(-Inf,n.alpha)),ub=c(Inf,1.99,rep(Inf,n.alpha)),hessian=FALSE,opt=TRUE,trace=TRUE,step2=TRUE,idx.para=1:2)
 
+results21 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=results2$par,fixed=c(F,F,rep(F,n.alpha)),basis=basis,thres=thres,model="logskew",maxit=1000,FUN=vario.func,alpha.func=alpha.func,ncores=ncores,method="Nelder-Mead",lb=c(0.01,0.0,rep(-Inf,n.alpha)),ub=c(Inf,1.99,rep(Inf,n.alpha)),hessian=TRUE,opt=FALSE,trace=TRUE,step2=FALSE,idx.para=1:2)
 results21 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=results2$par,fixed=c(F,F,rep(F,n.alpha)),basis=basis,thres=thres,model="logskew",maxit=1000,FUN=vario.func,alpha.func=alpha.func,ncores=ncores,method="Nelder-Mead",lb=c(0.01,0.0,rep(-Inf,n.alpha)),ub=c(Inf,1.99,rep(Inf,n.alpha)),hessian=TRUE,opt=FALSE,trace=TRUE,step2=FALSE,idx.para=1:2)
 
 U = eigen(e$results21$K)$values
@@ -55,23 +57,21 @@ sqrt(diag(K.inv %*% e$results21$hessian %*% K.inv))
 
 results4 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=init[idx.para],fixed=c(F,F),thres=thres,model="BR",maxit=1000,FUN=vario.func,ncores=ncores,method="Nelder-Mead",lb=c(0.01,0.0),ub=c(Inf,1.99),hessian=FALSE,opt=TRUE,trace=TRUE,idx.para=1:2)
 
-results5 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=init[idx.para],fixed=c(F,T),thres=thres,model="BR",maxit=1000,FUN=vario.func,ncores=ncores,method="Nelder-Mead",lb=c(0.01,0.0),ub=c(Inf,1.99),hessian=FALSE,opt=TRUE,trace=TRUE,idx.para=1:2)
-
 results41 <- fit.model(data=maxima.frechet,loc=loc.sub.trans,init=results4$par[idx.para],fixed=c(F,F),thres=thres,model="BR",maxit=1000,FUN=vario.func,ncores=ncores,method="L-BFGS-B",lb=c(0.01,0.0),ub=c(Inf,1.99),hessian=TRUE,opt=FALSE,trace=TRUE,idx.para=1:2)
 
 
 sqrt(diag(solve(e$results41$K) %*% e$results41$hessian %*% solve(e$results41$K)))
 
-save.image(file=paste0(DataPath,"data/application_results_new_.RData"))
+save.image(file=paste0(DataPath,"data/application_results_new_2.RData"))
 
 ### jackknife results ###
 data.avg = rowMeans(maxima.frechet)
 data = maxima.frechet[data.avg>thres,]
 
-results22 <- mclapply(1:nrow(data),function(i){fit.model(data=data[-i,],loc=loc.sub.trans,init=results2$par,fixed=c(F,F,rep(F,n.alpha)),basis=basis,thres=thres,model="logskew",maxit=1000,FUN=vario.func,alpha.func=alpha.func,ncores=NULL,method="L-BFGS-B",lb=c(0.01,0.0,rep(-Inf,n.alpha)),ub=c(Inf,1.99,rep(Inf,n.alpha)),hessian=FALSE,opt=TRUE,trace=TRUE,step2=FALSE,idx.para=1:2)},mc.cores=ncores,mc.set.seed = FALSE) 
+results22 <- mclapply(1:nrow(data),function(i){fit.model(data=data[-i,],loc=loc.sub.trans,init=results2$par,fixed=c(F,F,rep(F,n.alpha)),basis=basis,thres=thres,model="logskew",maxit=1000,FUN=vario.func,alpha.func=alpha.func,ncores=NULL,method="Nelder-Mead",lb=c(0.01,0.0,rep(-Inf,n.alpha)),ub=c(Inf,1.99,rep(Inf,n.alpha)),hessian=FALSE,opt=TRUE,trace=TRUE,step2=FALSE,idx.para=1:2)},mc.cores=ncores,mc.set.seed = FALSE) 
 
-results42 <- mclapply(1:nrow(data),function(i){fit.model(data=data[-i,],loc=loc.sub.trans,init=results4$par[idx.para],fixed=c(F,F),thres=thres,model="BR",maxit=1000,FUN=vario.func,ncores=NULL,method="L-BFGS-B",lb=c(0.01,0.0),ub=c(Inf,1.99),hessian=FALSE,opt=TRUE,trace=TRUE,idx.para=1:2)},mc.cores=ncores,mc.set.seed = FALSE) 
+results42 <- mclapply(1:nrow(data),function(i){fit.model(data=data[-i,],loc=loc.sub.trans,init=results4$par[idx.para],fixed=c(F,F),thres=thres,model="BR",maxit=1000,FUN=vario.func,ncores=NULL,method="Nelder-Mead",lb=c(0.01,0.0),ub=c(Inf,1.99),hessian=FALSE,opt=TRUE,trace=TRUE,idx.para=1:2)},mc.cores=ncores,mc.set.seed = FALSE) 
 
-save.image(file=paste0(DataPath,"data/application_results_new_.RData"))
+save.image(file=paste0(DataPath,"data/application_results_new_2.RData"))
 
 
