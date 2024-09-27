10d934557e3103f9c140611852efbd2d506e42f4
PangChung <zhongprw@gmail.com>
2024-09-27 10:55:04 +1000

    update

--- code/simulation_study_pareto.R
+++ code/simulation_study_pareto.R
@@ -88,7 +88,7 @@ model.fit <- function(i){
     data = data[data.sum>u,]/u
     t0 <-  proc.time()
     fit.result1 <- fit.scoreMatching(init=init, obs=data, loc=coord, fixed=c(F,F,T,T,T),lb=lb,ub=ub, model="logskew", vario.func=vario.func, idx.para=idx.para, alpha.func=alpha.func, basis=basis, weightFun = weightFun, dWeightFun = dWeightFun, method="Nelder-Mead", maxit=1000, ncores = NULL)
-    fit.result1 <- fit.scoreMatching(init=fit.result1$par, obs=data, loc=coord, fixed=c(F,F,F,F,F),lb=lb,ub=ub, model="logskew", vario.func=vario.func, idx.para=idx.para, alpha.func=alpha.func, basis=basis, weightFun = weightFun, dWeightFun = dWeightFun, method="Nelder-Mead", maxit=1000, ncores = NULL)
+    fit.result1 <- fit.scoreMatching(init=fit.result1$par, obs=data, loc=coord, fixed=c(F,F,T,F,F),lb=lb,ub=ub, model="logskew", vario.func=vario.func, idx.para=idx.para, alpha.func=alpha.func, basis=basis, weightFun = weightFun, dWeightFun = dWeightFun, method="Nelder-Mead", maxit=1000, ncores = NULL)
     t0 <- proc.time() - t0
     fit.result1$time <- t0
 
@@ -96,7 +96,7 @@ model.fit <- function(i){
     data.sum = apply(data,1,sum)
     u = quantile(data.sum,0.95)
     data = data[data.sum>u,]/u
-    fit.result2 <- fit.model(data=data,loc=coord,init=init,fixed=c(F,F,F,F,F),basis=basis,thres=0,model="logskew",FUN=vario.func,alpha.func=alpha.func,ncores=NULL,maxit=1000,method="Nelder-Mead",lb=lb,ub=ub,hessian=FALSE,opt=TRUE,trace=FALSE,step2=FALSE,idx.para=idx.para,pareto=TRUE)
+    fit.result2 <- fit.model(data=data,loc=coord,init=init,fixed=c(F,F,T,F,F),basis=basis,thres=0,model="logskew",FUN=vario.func,alpha.func=alpha.func,ncores=1,maxit=1000,method="Nelder-Mead",lb=lb,ub=ub,hessian=FALSE,opt=TRUE,trace=FALSE,step2=FALSE,idx.para=idx.para,pareto=TRUE)
 
     return(list(fit.result1,fit.result2))
 }
