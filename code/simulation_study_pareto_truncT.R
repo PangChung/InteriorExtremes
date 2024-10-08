59f1a6eb7c1798ca5e342c0ba34c8d6ff4fe8e86
PangChung <zhongprw@gmail.com>
2024-10-08 12:49:01 +1100

    update

--- code/simulation_study_pareto_truncT.R
+++ code/simulation_study_pareto_truncT.R
@@ -81,6 +81,7 @@ model.fit <- function(i){
 
     fit.result2 <- fit.model(data=data,loc=diff.mat,init=init,fixed=c(F,F,T),thres=0,model="truncT",FUN=cov.func,maxit=1000,method="L-BFGS-B",lb=lb,ub=ub,hessian=FALSE,opt=TRUE,trace=FALSE,idx.para=idx.para,pareto=TRUE,ncores=3)
     
+    fit.result2$par = fit.result2$par[-3]
     return(list(fit.result1,fit.result2))
 }
 
