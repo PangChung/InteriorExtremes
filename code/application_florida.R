cdec18b218e19231dc94cf22e5bb8be592dcf349
PangChung <zhongprw@gmail.com>
2024-10-16 10:25:10 +1100

    update

---
+++ code/application_florida.R
@@ -0,0 +1,34 @@
+rm(list=ls())
+args <- commandArgs(TRUE)
+source("code/simulation.R")
+source("code/exponent_functions.R")
+source("code/likelihood_inference.R")
+computer = "local"
+for (arg in args) eval(parse(text = arg))
+switch(computer,
+    "ws" = {DataPath<-"~/Desktop/InteriorExtremes/"},
+    "hpc" = {DataPath<-"/srv/scratch/z3536974/";.libPaths("../src")},
+    "local" = {DataPath<-"~/Documents/Github/InteriorExtremes/"}
+)
+library(parallel)
+library(mvtnorm)
+library(TruncatedNormal)
+library(evd)
+library(partitions)
+library(Rfast)
+library(matrixStats)
+set.seed(12342)
+## load the data##
+load("data/data_application.RData")
+D = ncol(maxima.frechet)
+ncores = floor(detectCores()/2)
+idx.centers = unlist(lapply(quantile(loc.sub.trans[,1],seq(0.1,0.9,length.out=5)),function(x){ idx = abs(loc.sub.trans[,1] - x) < 5; which(idx)[which.min(abs(loc.sub.trans[idx,2] - median(loc.sub.trans[idx,2])))]}))
+
+basis <- sapply(idx.centers,function(x){y=dnorm(distmat[x,],mean=0,sd=ncol(distmat)*2);y=y-mean(y);y/sqrt(sum(y^2))})
+
+init = c(100,1,rep(0,ncol(basis)))
+n.alpha = ncol(basis)
+idx.para = 1:2
+
+thres = 10
+
