ba62db2124a751c3b3d871545eb5da8c4da911e8
PangChung <zhongprw@gmail.com>
2024-10-22 15:03:56 +1100

    update

--- code/katana2.sh
+++ code/katana2.sh
@@ -14,7 +14,7 @@ module load gsl/2.7.1
 module load gmp/6.2.1 
 module load r/4.3.1
 
-Rscript $script "${inputs};computer=\"hpc\"" 
+Rscript $script "computer=\"hpc\"" 
 #Rscript code/application.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\""
 # Rscript code/application.R "computer=\"hpc\""
 # Rscript code/simulation_studies_comp.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\";d=15;m=100"
