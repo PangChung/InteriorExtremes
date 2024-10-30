440d7bfb68172edaa092fafbb91eb75952a4229c
PangChung <zhongprw@gmail.com>
2024-10-30 20:38:10 +1100

    update

--- code/katana2.sh
+++ code/katana2.sh
@@ -4,7 +4,7 @@
 #PBS -l mem=124gb
 #PBS -l walltime=12:00:00
 #PBS -j oe 
-#PBS -J 1-70
+#PBS -J 3-4
 ##PBS -m ae
 ##PBS -M peng.zhong@unsw.edu.au 
 
@@ -15,7 +15,7 @@ module load gmp/6.2.1
 module load r/4.3.1
 
 #Rscript $script "computer=\"hpc\";id=${PBS_ARRAY_INDEX}" 
-Rscript code/application_florida.R "computer=\"hpc\";idx.jack=${PBS_ARRAY_INDEX};method=\"L-BFGS-B\";id=${id}" 
+Rscript code/application_florida.R "computer=\"hpc\";idx.jack=${PBS_ARRAY_INDEX};method=\"Nelder-Mead\";id=${id}" 
 # Rscript code/application_florida.R "computer=\"hpc\";idx.jack=0;method=\"L-BFGS-B\";id=${PBS_ARRAY_INDEX}" 
 #Rscript code/application.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\""
 # Rscript code/application.R "computer=\"hpc\""
