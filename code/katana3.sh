41537d141cb327e3507b9bc5959bade4ef9f5b48
PangChung <zhongprw@gmail.com>
2025-01-29 00:39:23 +1100

    update

--- code/katana3.sh
+++ code/katana3.sh
@@ -16,8 +16,8 @@ module load r/4.3.1
 
 # Rscript $script
 # Rscript code/application_florida.R "computer=\"hpc\";idx.jack=${PBS_ARRAY_INDEX};method=\"Nelder-Mead\";id=${id}" 
-# Rscript $script "computer=\"hpc\";idx.jack=${PBS_ARRAY_INDEX};method=\"L-BFGS-B\";id=${id}" 
+Rscript $script "computer=\"hpc\";idx.jack=${PBS_ARRAY_INDEX};method=\"Nelder-Mead\";id=${id}" 
 # Rscript $script "${inputs};id=${PBS_ARRAY_INDEX};computer=\"hpc\"" 
 # Rscript code/simulation_studies_comp.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\";d=15;m=500"
 # Rscript code/simulation_studies_comp2.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\";d=15;m=500"
-Rscript code/simulation_studies_comp2_2.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\";d=15;m=500"
+# Rscript code/simulation_studies_comp2_2.R "id=${PBS_ARRAY_INDEX};computer=\"hpc\";d=15;m=500"
